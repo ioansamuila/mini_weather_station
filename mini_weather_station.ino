42d18dce65c3533d25334db5104f5eceac3f48b7
Ioan Samuilă <ioan.samuila73@e-uvt.ro>
2018-06-01 23:36:36 +0300

    Add more features
    Create two function. One for transform from Celsius to Fahrenheit and another to transform from Celsius to Kelvin.
    Add some comments.

--- mini_weather_station.ino
+++ mini_weather_station.ino
@@ -2,15 +2,42 @@
 #include <dht.h>
 
 dht DHT;
-
+// initialize the library with the numbers of the interface pins
 LiquidCrystal lcd(12, 11, 5, 4, 3, 2);
 
+//Celsius to Fahrenheit conversion
+double Fahrenheit(double celsius)
+{
+        return 1.8 * celsius + 32;
+}
+
+//Celsius to Kelvin conversion
+double Kelvin(double celsius)
+{
+        return celsius + 273.15;
+}
+
 void setup() {
+  // set up the LCD's number of columns and rows:
   lcd.begin(16, 2);
+  Serial.begin(9600);
+  delay(500);//Delay to let system boot
+  Serial.println("DHT11 Humidity & temperature Sensor:");
+  delay(1000);//Wait before accessing Sensor
 }
 
 void loop() {
   DHT.read11(A2);
+  Serial.print("Current humidity = ");
+  Serial.print(DHT.humidity);
+  Serial.print("%  and ");
+  Serial.print("temperature = ");
+  Serial.print(DHT.temperature);
+  Serial.print("C / ");
+  Serial.print(Fahrenheit(DHT.temperature));
+  Serial.print("F / ");
+  Serial.print(Kelvin(DHT.temperature));
+  Serial.println("K.");
   lcd.clear();
   lcd.setCursor(1, 0);
   lcd.print("Hmdt.: ");
